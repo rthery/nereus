c0103df6c9bc5156b4a159a393d8209645e4e0c5
Romain Théry <romain.thery@ik.me>
2026-03-05 10:24:52 +0100

    Add deploy github workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_URL ?? '/',
   test: {
     environment: 'happy-dom',
     include: ['src/__tests__/**/*.test.ts'],
